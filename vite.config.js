b6cc36d8b81f3c8df02ec2397c5052837998f93f
17006827 <152843880+17006827@users.noreply.github.com>
2024-06-06 00:42:07 +0200

    configuring the vit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import data from "./data.json";
-// assert {type:'json'};
 import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from 'vite';
+import data from './data.json';
 
-export default {
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+            }
+        },
+    },
     plugins: [handlebars({
-      context : data
-    })]
-  };
+      context : {
+        data
+      }
+  })]
+    
+});
